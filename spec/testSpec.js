13e514bb2ef282dd8eb87e028b2c415b4e17ed11
Paul Gray <pfbgray@gmail.com>
2015-09-09 00:34:10 -0400

    Fixing some tests

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -24,34 +24,52 @@ describe('translate()', function () {
     function getNewEntityWithAttr(guid, obj){
       var entity = clone(baseEntity);
       function populateAttrs(attributeSet){
-        _.each(["use", "require"], function(attr){
-          attributeSet[attr] = obj;
+        ["use", "require"].forEach(function(attr){
+          attributeSet[attr][guid] = obj;
         });
       }
       populateAttrs(entity.original);
-      _.each(entity.journal, populateAttrs);
+      entity.journal.map(populateAttrs);
       return entity;
     }
-
-    it('should translate the "title" attribute', function(){
-      var entity = getNewEntityWithAttr(
-        "1f2625c2-615f-11e3-bf13-d231feb1dc81",
-        "Title"
-      );
+    
+    /**
+    * This function will test that casa.translate() will translate an attribute
+    * with the supplied guid.
+    */
+    function testPair(guid, human, obj){
+      var entity = getNewEntityWithAttr(guid, obj);
       var translated = casa.translate()(entity);
-      console.log('got: ', translated);
-      expect(translated.original.use.title).toBe("Title");
-      /*
-        "1f2625c2-615f-11e3-bf13-d231feb1dc81":"Statistical Mechanics",
-        "d59e3a1f-c034-4309-a282-60228089194e":[{
-          "name":"Massachusetts Institute of Technology"
-        }],
-        "b7856963-4078-4698-8e95-8feceafe78da":"asdf"
-      */
-      
+      if(typeof translated.original.use[human] === 'undefined'){
+        throw Error('Translated attribute was undefined');
+      }
+      expect(translated.original.use[human]).toBe(obj);
+    }
+    
+    it('should translate the "title" attribute', function(){
+      testPair(
+        '1f2625c2-615f-11e3-bf13-d231feb1dc81',
+        'title',
+        'Title of the App'
+      );
     });
 
     it('should translate the "description" attribute', function(){
-      
+      testPair(
+        'b7856963-4078-4698-8e95-8feceafe78da',
+        'description',
+        'Description of the app'
+      );
     });
+
+    it('should translate the "authors" attribute', function(){
+      testPair(
+        'd59e3a1f-c034-4309-a282-60228089194e',
+        'authors',{
+          "name":"Massachusetts Institute of Technology"
+        }
+      );
+    });
+    
+    
 });
